e12f8c03886c4f4f3d4c83c1b675e4c629d27ffa
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-09-13 22:47:29 +0800

    adding code generator graphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,25 @@
-import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core'
-
+import React from "react";
+import {createClient, Provider} from "urql";
+import {ThemeProvider, CSSReset, ColorModeProvider} from '@chakra-ui/core'
 import theme from '../theme'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider theme={theme}>
-      <ColorModeProvider>
-        <CSSReset />
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ThemeProvider>
-  )
+const client = createClient({
+    url: "http://localhost:4000/graphql", fetchOptions: {
+        credentials: "include"
+    }
+});
+
+function MyApp({Component, pageProps}) {
+    return (
+        <Provider value={client}>
+            <ThemeProvider theme={theme}>
+                <ColorModeProvider>
+                    <CSSReset/>
+                    <Component {...pageProps} />
+                </ColorModeProvider>
+            </ThemeProvider>
+        </Provider>
+    )
 }
 
 export default MyApp
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 import {Formik, Form} from 'formik'
-import { Box, Button} from "@chakra-ui/core/dist";
+import {Box, Button} from "@chakra-ui/core/dist";
 import Wrapper from "../components/Wrapper";
 import {InputField} from "../components/InputField";
+import {useRegisterMutation} from "../generated/graphql";
 
 
 interface registerProps {
-
 }
 
 const Register: React.FC<registerProps> = ({}) => {
+    const [, register] = useRegisterMutation();
     return (
         <Wrapper variant={"small"}>
-            <Formik initialValues={{username: "", password: ""}} onSubmit={(values) => {
-                console.log(values)
-            }}>{({isSubmitting}) => (
+            <Formik initialValues={{username: "", password: ""}}
+                    onSubmit={async (values) => {
+                        const response = await register(values);
+
+                    }}>{({isSubmitting}) => (
                 <Form>
-                    <InputField name={"username"} label={"Username"} placeholder={"username"} />
+                    <InputField name={"username"} label={"Username"} placeholder={"username"}/>
                     <Box mt={4}>
-                        <InputField name={"password"} label={"Password"} placeholder={"password"} type={"password"} />
+                        <InputField name={"password"} label={"Password"} placeholder={"password"} type={"password"}/>
                     </Box>
                     <Button
                         mt={4}
